c540240be3f023130176bdd7614ef5a3b7a25a83
Jorge <110149668+JorgeMRs@users.noreply.github.com>
2023-09-25 19:53:19 +0000

    falta

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,3 +1,17 @@
+#! /usr/bin/env python3.6
+"""
+Python 3.6 or newer required.
+"""
+# import json
+# import os
+# import stripe
+
+# This is your test secret API key.
+# stripe.api_key = 'sk_test_51Nsr4fKXj5LWRngy31gxXDgOiRztmNpiBBmqDpLBRuqDHNdfDIbOG9aT56ZppZYviuhqit7eKlKFZnjmFwxgiyjZ00Jvx3IxRM'
+
+
+
+
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
@@ -11,6 +25,45 @@ api = Blueprint('api', __name__)
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'tu_clave_secreta'
 jwt = JWTManager(app) 
+
+# CORS(api)
+# stripe.api_key = 'sk_test_51Nsr4fKXj5LWRngy31gxXDgOiRztmNpiBBmqDpLBRuqDHNdfDIbOG9aT56ZppZYviuhqit7eKlKFZnjmFwxgiyjZ00Jvx3IxRM'
+
+
+
+# ----------------------------- ENDPOINTS USUARIOS -----------------------------------------------------------------
+
+
+
+# CORS(api,resources ={r"/create-payment-intent/*":{"origins":"https://super-duper-goldfish-gjrqrr4q9wpc9769-3000.app.github.dev"}})
+
+# def calculate_order_amount(items):
+#     # Replace this constant with a calculation of the order's amount
+#     # Calculate the order total on the server to prevent
+#     # people from directly manipulating the amount on the client
+#     return 1400
+
+# # @cross_origin
+# @api.route('/create-payment-intent', methods=['POST'])
+# def create_payment():
+#     try:
+#         data = json.loads(request.data)
+#         # Create a PaymentIntent with the order amount and currency
+#         intent = stripe.PaymentIntent.create(
+#             amount=200,
+#             currency='USD',
+#             # In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+#             automatic_payment_methods={
+#                 'enabled': True,
+#             },
+#         )
+#         return jsonify({
+#             'clientSecret': intent['client_secret']
+#         })
+#     except Exception as e:
+#         return jsonify(error=str(e)), 403
+
+# ----------------------------- ENDPOINTS USUARIOS -----------------------------------------------------------------
 
 
 @api.route('/hello', methods=['POST', 'GET'])
